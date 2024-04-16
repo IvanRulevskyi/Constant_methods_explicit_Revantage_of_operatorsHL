ec2cfe899953c9584e226226440764a2df60c7c5
IvanRulevskyi <rulevskiy4@gmail.com>
2024-04-16 08:11:50 +0300

    Initial Commit

---
+++ Constant_methods_explicit_Revantage_of_operatorsHL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Constant_methods_explicit_Revantage_of_operatorsHL
+//
+//  Created by Иван Рулевский on 16.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
